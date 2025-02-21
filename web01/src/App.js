18dd2b0663613fbd2c21f74aca97bedf1477f82e
Lorenzopedro.freitas <98405763+lorenzopedro@users.noreply.github.com>
2025-02-21 22:31:05 +0000

    primeiro commit

---
+++ web01/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const nome = "Lorenzo Freitas";
+
+const elemento = (
+  <div>
+    <h1>Oi, eu sou o {nome}</h1>
+    <p>Aluno do curso de Sistemas de INformação!!!</p>
+  </div>
+);
+
+function App(){
+  return elemento;
+}
+
+export default App;
